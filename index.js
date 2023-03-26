17032bfe1c1ea38445a62455c9d62a4bd1143fb4
MarksBeejay <121988969+MarksBeejay@users.noreply.github.com>
2023-03-26 19:32:48 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const name = 'Makanjuola Bolaji';
+const language = 'JavaScript';
+const goal = 'Senior Software Engineer';
+
+console.log('My name is ' + name + '. My preferred programming language is ' + language + '. In the next 1 year, I see myself as a ' + goal + ' at one of the top airlines in the world.');
